95965c8bd7898d440918b872940fb4b95fb041bd
Kang-min Liu <gugod@gugod.org>
2015-04-29 21:53:51 +0200

    merge ScssGrammer grammar

--- lib/Scss.pm
+++ lib/Scss.pm
@@ -1,4 +1,43 @@
-use ScssGrammar;
+my grammar ScssGrammar {
+    rule TOP {
+        (<import_statement> | <scssrule>)*
+    }
+
+    rule import_statement {
+        "@import" <-[\n]>+ ";"
+    }
+
+    rule scssrule {
+        <selector> '{'
+        ( <property_kv> | <scssrule> )* %% ';'
+        '}'
+    }
+
+    rule selector {
+        (<tag_selector> (<id_selector> | <class_selector>)* | (<id_selector> | <class_selector>)+)+ %% ','
+    }
+
+    rule property_kv {
+        <property_name> ':' <property_value>
+    }
+
+    token tag_selector { <.ident> }
+
+    token property_name {
+        '-'?<alpha>(<.ident> | '-')*
+    }
+
+    token property_value { <-[;]>+ }
+    
+    token id_selector {
+        "#" <.ident>+
+    }
+
+    token class_selector {
+        "." <.ident>+
+    }
+
+}
 
 class Scss {
     method convert( Str $scss_text ) {
